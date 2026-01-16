2123fc5a5d521826264cba8bc970091fdd88c853
niishaparashar <nishaparashar1712@gmail.com>
2026-01-16 16:36:47 +0530

    Backend: AIS ingestion and vessel APIs

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     
 
     "users",
@@ -139,7 +140,10 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
+
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
 }
 
 SIMPLE_JWT = {
